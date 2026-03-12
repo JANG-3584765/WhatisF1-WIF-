4368ad6c397e0dad11a62f0eb95cc52dc07d456d
JANG-3584765 <wonjun001227@gmail.com>
2026-03-12 23:28:57 +0900

    login close function

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,19 @@ function App() {
     setIsLoginOpen(true);
   };
 
+   /* 로그인 닫기 함수 */
+  const closeLogin = () => {
+    setIsLoginOpen(false);
+  };
+
   /* - Header 컴포넌트에 로그인 열기 함수를 props로 전달
      - true일 때만 로그인 모달이 보이도록 조건부 렌더링 */
   return (
     <div>
       <Header openLogin={openLogin} /> 
-      {isLoginOpen && <LoginModal />}
+      {isLoginOpen && (
+      <LoginModal closeLogin={closeLogin} />
+      )}
     </div>
   )
 }
--- src/components/Header/Login/LoginModal.jsx
+++ src/components/Header/Login/LoginModal.jsx
@@ -1,15 +1,20 @@
-function LoginModal() {
+function LoginModal({ closeLogin }) {
   return (
     <div className="login-modal">
+
       <div className="modal-content">
 
-        <h2>Login</h2>
+        <div className="modal-header">
+          <h2>Login</h2>
+
+          <button onClick={closeLogin}>
+            X
+          </button>
+        </div>
 
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-
         <button>Login</button>
-
         <hr />
 
         <button>Login with Google</button>
