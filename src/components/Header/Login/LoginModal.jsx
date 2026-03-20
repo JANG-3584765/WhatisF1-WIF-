9d2e474acf916abb65a4f547078aeb7717193e94
JANG-3584765 <wonjun001227@gmail.com>
2026-03-20 21:57:53 +0900

    login function add

--- src/components/Header/Login/LoginModal.jsx
+++ src/components/Header/Login/LoginModal.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import "./LoginModal.css";
 
-function LoginModal({ closeLogin }) {
+function LoginModal({ closeLogin }) { // closeLogin: 부모 컴포넌트(App)에서 전달받은 로그인 닫기 함수
+  const [email, setEmail] = useState("");
+  // email: 현재 이메일 값, setEmail: 이메일 값 변경 함수
+  const [password, setPassword] = useState("");
+  // password: 현재 비밀번호 값, setPassword: 비밀번호 값 변경 함수
+
+  function handleLogin() { // 로그인 처리 함수
+    // 공백 대비: alert 실행 및 로그인 제한
+    if(!email) {
+      alert("이메일을 입력해주세요.");
+      return;
+    }
+    if(!password) {
+      alert("비밀번호를 입력해주세요.");
+      return;
+    }
+    console.log("email:", email);
+    console.log("password:", password);
+    closeLogin(); // 로그인 후 모달 닫기
+  }
+
   return (
     <div className="login-modal" onClick={closeLogin}>
 
@@ -17,20 +38,39 @@ function LoginModal({ closeLogin }) {
           </button>
         </div>
 
-        <div className="login-form">
+          <div className="login-form">
+          
+          {/*사용자 입력 -> onChange 발생 ->
+          setEmail/Password 실행 -> React State 업데이트 */}
+          
+          {/* 이메일 입력 필드 */}
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+          {/* 비밀번호 입력 필드 */}
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <button className="login-button">
+          <button 
+            className="login-button"
+            onClick={handleLogin} // 로그인 처리 함수 버튼 연결 
+          >
           로그인
-        </button>
+          </button>
 
-        </div>
+          </div>
 
-        <hr />
+          <hr />
 
-        <div className="social-login">
+          <div className="social-login">
           <button className="google-login">
           Google로 계속하기
           </button>
@@ -38,7 +78,7 @@ function LoginModal({ closeLogin }) {
           <button className="kakao-login">
           Kakao로 계속하기
           </button>
-        </div>
+          </div>
 
       </div>
 
